8198249546a4dd35d4a63719e327ae79827a6cf6
Marina1232 <marina.saiko.18@gmail.com>
2022-07-26 20:00:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  while (true) {
+    const newArrayBrackets = bracketsConfig.map((item) => {
+      return item.join("");
+    });
+
+    let newLength = str.length;
+
+    for (let i = 0; i < newArrayBrackets.length; i++) {
+      str = str.replace(newArrayBrackets[i], "");
+    }
+
+    if (str.length === 0 || newLength === str.length) {
+      break;
+    }
+  }
+  return str.length === 0;
+};
